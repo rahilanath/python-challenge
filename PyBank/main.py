329dda81de444a44f649937648f3828748f30e57
Rahil Nath <rahilanath@gmail.com>
2020-07-11 16:30:58 -0700

    final upload of scripts and txt files

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,57 +8,46 @@ bugdet_csv = os.path.join("Resources","budget_data.csv")
 analysis_file = os.path.join("analysis","analysis_report.txt")
 
 # function that runs analysis
-def run_analysis(input_list, input_rows):
+def run_analysis(input_rows, input_length):
     # initialize variables used in iteration and list for monthly changes
-    profit = 0.00
-    loss = 0.00
-    increase = 0
-    decrease = 0
+    greatest_increase = 0
+    greatest_decrease = 0
     changes = []
+    net_profit = 0.00
 
-    # iterate through list argument
-    for row in range(0, input_rows):
-
-        # sums profit month over month
-        if float(input_list[row][1]) > 0:
-            profit = profit + float(input_list[row][1])
+    # iterate through list argument ignoring first month
+    for row in range(1, input_length):
         
-        # sums loss month over month
-        elif float(input_list[row][1]) < 0:
-            loss = loss + float(input_list[row][1])
+        #sums net profit
+        net_profit = net_profit + float(input_rows[row][1])
         
-        # calculates change from previous month per iteration and stores in changes list
-        if row > 0:
-            changes.append(float(input_list[row][1]) - float(input_list[row-1][1]))
+        # calculates hmonth over month change per iteration and stores in list
+        changes.append(float(input_rows[row][1]) - float(input_rows[row-1][1]))
 
-            # updates value for increase and decrease iteration variables
-            if float(input_list[row][1]) > float(input_list[increase][1]):
-                increase = row
-                incmonth = input_list[row][0]
+    # finds value for greatest increase and decrease and sets variables
+        for row in range(0, len(changes)):
+            if float(changes[row]) > greatest_increase:
+                greatest_increase = changes[row]
+                month_increase = input_rows[row][0]
             
-            elif float(input_list[row][1]) < float(input_list[decrease][1]):
-                decrease = row
-                decmonth = input_list[row][0]
+            elif float(changes[row]) < greatest_decrease:
+                greatest_decrease = changes[row]
+                month_decrease = input_rows[row][0]
     
-    # run calculations and set variables for print statements
-    net = profit + loss
-    avgchange = sum(changes)/len(changes)
-    greatest_increase = float(input_list[increase][1])
-    greatest_decrease = float(input_list[decrease][1])
-    month_increase = input_list[increase][0]
-    month_decrease = input_list[decrease][0]
+    # run remaining calculations
+    avg_change = sum(changes)/len(changes)
 
-    # create print statements
-    title = 'Financial Analysis'
-    spacer = '----------------------------'
-    total_months = f"Total Months: {input_length}"
-    net = f"Total: ${net:.2f}"
-    avg_change = f"Average Change: ${avgchange:.2f}"
-    month_increase = f"Greatest Increase in Profits: {month_increase} (${greatest_increase:.2f})"
-    month_decrease = f"Greatest Decrease in Profits: {month_decrease} (${greatest_decrease:.2f})"
+    # create print statements using variables
+    print_title = 'Financial Analysis'
+    print_spacer = '----------------------------'
+    print_months = f"Total Months: {input_length}"
+    print_net = f"Total: ${net_profit:.2f}"
+    print_change = f"Average Change: ${avg_change:.2f}"
+    print_increase = f"Greatest Increase in Profits: {month_increase} (${greatest_increase:.2f})"
+    print_decrease = f"Greatest Decrease in Profits: {month_decrease} (${greatest_decrease:.2f})"
 
     # return list with print statements
-    return (title, spacer, total_months, net, avg_change, month_increase, month_decrease)
+    return (print_title, print_spacer, print_months, print_net, print_change, print_increase, print_decrease)
 
 # open budget file and creates list with rows and row count
 with open(bugdet_csv, 'r') as input_csv:
@@ -82,7 +71,7 @@ with open(analysis_file, 'w', newline='') as output_file:
         print(result)
     
     # removes last line empty line from file to make OCD happy
-    # sample code from stack overflow:
+    # original code from stack overflow:
     # https://stackoverflow.com/questions/53086588/delete-last-and-blank-line-from-file-written-by-csv-writer
     output_file.seek(0, os.SEEK_END)
     output_file.seek(output_file.tell()-2, os.SEEK_SET)
---
+++ PyPoll/main.py
@@ -0,0 +1,96 @@
+import os
+import csv
+
+# path to budget file
+election_csv = os.path.join("Resources","election_data.csv")
+
+# path to analysis report file
+analysis_file = os.path.join("analysis","analysis_report.txt")
+
+# function to dynamically create list of ballot results template
+def ballot_results_template(input_rows, input_length):
+    # initialize list variables
+    candidate_list = []
+    results_template = []
+
+    # find distinct candidate names
+    for row in range(0, input_length):
+        if input_rows[row][2] not in candidate_list:
+            candidate_list.append(input_rows[row][2])
+
+    # create ballot results template list with initialized vote count
+    for candidate in candidate_list:
+        results_template.append([candidate, 0, ''])
+
+    # return ballot results template list
+    return results_template
+
+# function that runs analysis
+def run_analysis(input_rows, input_length):
+    # # call function to dynamically create a unique candidate list
+    ballot_results = ballot_results_template(input_rows, input_length)
+
+    # count votes from input list and store in ballot results template list
+    for row in range(0, input_length):
+        for candidate in range(0, len(ballot_results)): 
+            if ballot_results[candidate][0] == input_rows[row][2]:
+                ballot_results[candidate][1] = ballot_results[candidate][1] + 1
+
+    # find winner and mark in results
+    for row in range(0, len(ballot_results)):
+        if ballot_results[row][1] > ballot_results[row-1][1]:
+            ballot_results[row][2] = 'Winner'
+            ballot_results[row-1][2] = ''
+    
+    # return ballot results list
+    return (ballot_results)
+
+# open budget file and creates list with rows and row count
+with open(election_csv, 'r') as input_csv:
+    input_reader = csv.reader(input_csv, delimiter=',')
+    input_header = next(input_reader)
+    input_rows = list(input_reader)
+    input_length = len(input_rows)
+
+    # pass list of row values to function and set variable for return list
+    results = run_analysis(input_rows, input_length)
+
+# open analysis report file, newline parameter to remove carriage
+with open(analysis_file, 'w', newline='') as output_file:
+    output_writer = csv.writer(output_file, delimiter=',')
+
+    # set print statements
+    title = 'Election Results'
+    spacer = '----------------------------'
+
+    # write/print title and spacer
+    output_writer.writerow([title])
+    output_writer.writerow([spacer])
+    print(title)
+    print(spacer)
+
+    # dynamically write/print results
+    for row in range(0, len(results)):
+        output_writer.writerow([f'{results[row][0]}: {(results[row][1]/input_length):.3%} ({results[row][1]})'])
+        print(f'{results[row][0]}: {(results[row][1]/input_length):.3%} ({results[row][1]})')
+
+    # write/print spacer
+    output_writer.writerow([spacer])
+    print(spacer)
+
+    # write/print winner
+    for row in range(0, len(results)):
+        if results[row][2] == 'Winner':
+            output_writer.writerow([f'Winner: {results[row][2]}'])
+            print(f'Winner: {results[row][0]}')
+        
+    # write/print final spacer
+    output_writer.writerow([spacer])
+    print(spacer)
+    
+    # # removes last line empty line from file to make OCD happy
+    # # original code from stack overflow:
+    # # https://stackoverflow.com/questions/53086588/delete-last-and-blank-line-from-file-written-by-csv-writer
+    # output_file.seek(0, os.SEEK_END)
+    # output_file.seek(output_file.tell()-2, os.SEEK_SET)
+    # output_file.truncate()
